25bed98f181345c874e544306f709861933f5c97
lucashspiess <lucashs725@gmail.com>
2025-06-03 09:42:14 -0300

    contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'last_name', 'created_date',
+    ordering = '-id',
